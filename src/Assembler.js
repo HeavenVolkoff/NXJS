e272de37bd8d0357f0750e35158f7b8377d18d7f
Vitor Augusto da Silva Vasconcellos <vasvas10@gmail.com>
2016-06-02 23:39:38 -0300

    Implemented basic compiler error generation small test case

--- src/Assembler.js
+++ src/Assembler.js
@@ -14,11 +14,12 @@ class Assembler{
         this.memoryIndex   = 0;
         this.memory        = new Uint8Array(255);
         this.labels        = new Map();
+        this.string        = str;
         this.code          = [];
         this.map           = [];
 
-        const spaceRegexExp = util.regExps.spaceRegexExp;
-        const opCodeRegex   = util.regExps.opCodeRegex;
+        const spaceRegexExp = util.regExps.space;
+        const opCodeRegex   = util.regExps.opCode;
 
         let i = 0;
 
@@ -34,6 +35,9 @@ class Assembler{
                 }else if(spaceRegexExp.test(str[j])){
                     j++;
                     break;
+
+                }else if(str[j] === ';'){
+                    isComment = true;
 
                 }else {
                     snippet += str[j];
@@ -52,7 +56,7 @@ class Assembler{
                     this.code.push(snippet);
                 }
 
-                this.map.push([i - snippet.length, snippet.length]);
+                this.map.push(i - snippet.length);
             }
         }
 
@@ -63,8 +67,38 @@ class Assembler{
         //TODO: Loop through instructions array decoding and writing memory
     }
 
-    genErrorStack(line){
-        //TODO
+    genErrorStack(index){
+        const instructionIndex = this.map[index];
+        const lineBreakRegExp  = util.regExps.lineBreak;
+        
+        let lineStartIndex = 0,
+            lineEndIndex   = 0,
+            substring      = this.string.substring(0, instructionIndex),
+            column         = 0,
+            line           = 0;
+
+        //Reset RegExp
+        lineBreakRegExp.lastIndex = 0;
+
+        while(lineBreakRegExp.exec(substring) !== null) {
+            lineStartIndex = lineBreakRegExp.lastIndex;
+            line++;
+        }
+
+        console.log(line, lineStartIndex, instructionIndex);
+
+        //Get end lineIndex
+        lineBreakRegExp.lastIndex = lineStartIndex;
+        lineBreakRegExp.exec(this.string);
+
+        lineEndIndex = lineBreakRegExp.lastIndex - 1;
+        column       = instructionIndex - lineStartIndex;
+
+        return {
+            line: line,
+            column: column,
+            code: this.string.substring(lineStartIndex, lineEndIndex) + '\n' + ' '.repeat(instructionIndex - lineStartIndex) + '^\n'
+        };
     }
     
     EQU(index) {
@@ -75,7 +109,7 @@ class Assembler{
         let value = this.code[after];
         let temp;
 
-        if(!util.regExps.validLabelNameRegExp.test(label)){
+        if(!util.regExps.validLabelName.test(label)){
             throw new AssemblerError('INVALID_LABEL_NAME', this.genErrorStack(before));
         }
 
@@ -98,5 +132,9 @@ class Assembler{
         }
     }
 }
+
+//##Test Case
+let ass = new Assembler(';---------------------------------------------------\n; Programa: 6 -  Encontrar o menor elemento em um vetor de 10 elementos de 8 bits cada. Coloque os valores iniciais do vetor com uso das diretivas do montador. (1,0 ponto)\n; Autor: Raphael Almeida, Guilherme Freire e Vitor Augusto\n; Data: 26/04/2016\n;---------------------------------------------------\n\nMIN        EQU  197\nLOOP       EQU  198\nADDR       EQU  199\nV1         EQU  200\n\nORG  0 ; Inicializa o contador, LOOP, com o valor 10, O endereço inicial do vetor, V1, e aponta ADDR para o inicio do vetor.\n\n           LDI  10\n           STA  LOOP\n\n           LDI  200\n           STA  ADDR\n\n           LDI  10\n           STA  V1\n\n           JMP  30\n\n\nORG  30 ; Faz um loop decrementando LOOP (contador). Calcula o endereço da proxima posição do vetor em ADDR e armazena o valor da contagem (regressiva) nesse endereço. Preenchendo, assim, o vetor.\n\n           LDI  255\n           ADD  LOOP\n\n           JZ   90\n\n           STA  LOOP\n\n           LDI  1\n           ADD  ADDR\n           STA  ADDR\n\n           LDA  LOOP\n           STA  @ADDR\n\n           JMP  30                      \n\n\nORG 90 ; Reseta o contador, LOOP, com o valor 10, Aponta ADDR para o inicio do vetor e coloca o primeiro valor do vetor em MIN.\n\n           LDI  10\n           STA  LOOP\n\n           LDI  200\n           STA  ADDR\n\n           LDA  @ADDR\n           STA  MIN\n\n           JMP  120\n\n\nORG 120 ; Faz um loop decrementando LOOP (contador). Calcula o endereço da proxima posição do vetor em ADDR e se o valor for menor pula para o trecho 110.\n\n           LDI  255\n           ADD  LOOP\n\n           JZ   150\n\n           STA  LOOP\n\n           LDI  1\n           ADD  ADDR\n           STA  ADDR\n\n           LDA  MIN\n           SUB  @ADDR\n           JN   120\n\n           JMP  110\n\n\nORG 110 ; Armazena o valor na posição apontada por ADDR em MIN.\n\n           LDA  @ADDR\n           STA  MIN\n\n           JMP 120\n           \n\nORG 150 ; Mostra no visor o resultado final.\n\n          LDA  MIN\n          OUT  0\n          \n          HLT');
+console.log(ass.genErrorStack(20));
 
 module.exports = Assembler;
--- src/util.js
+++ src/util.js
@@ -16,7 +16,8 @@ exports.validNeanderNumber = (num) => {
 };
 
 exports.regExps = {
-    validLabelNameRegExp: /^[a-zA-Z\$_][a-zA-Z0-9\$_]*$/,
-    spaceRegexExp: /\s/,
-    opCodeRegex: /^NOP|STA|LDA|ADD|OR|AND|NOT|SUB|JMP|JN|JZ|JNZ|IN|OUT|LDI|HLT|ORG|EQU|END|DS|DB$/i
+    validLabelName: /^[a-zA-Z\$_][a-zA-Z0-9\$_]*$/,
+    lineBreak: /\r?\n|\r/g,
+    opCode: /^(NOP|STA|LDA|ADD|OR|AND|NOT|SUB|JMP|JN|JZ|JNZ|IN|OUT|LDI|HLT|ORG|EQU|END|DS|DB)$/i,
+    space: /\s/
 };
